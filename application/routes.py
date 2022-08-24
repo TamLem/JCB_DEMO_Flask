03116a9c17226622d738e91b2c6f64baa55d963f
Tamirat worku Lemma <tamworku1@gmail.com>
2022-08-24 21:54:14 +0200

    added backend logic and api endpoints

---
+++ application/routes.py
@@ -0,0 +1,25 @@
+import imp
+from application import app
+from flask import render_template, url_for, Response
+import pandas as pd
+import json 
+import plotly 
+import numpy as np
+import plotly.express as px
+
+def get_df():
+	df = pd.DataFrame(np.random.randint(0, 1000, 10), columns=['a'])
+	df['b'] = df['a'] % 10
+	return df
+
+@app.route('/data')
+def data():
+	#return raw data as json	
+	return Response(get_df().to_json(orient='values'), mimetype='application/json')
+
+@app.route('/api/data')
+def apiData():
+	#returns graph data as json
+	fig = px.scatter(get_df(), x='a', y='b', color='b', title="Random numbers vs modulo 10")
+	fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+	return Response(fig_json, mimetype='application/json')
