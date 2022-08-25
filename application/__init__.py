9523f48e049697c6fef76a2be42142918ebf36e0
Tamirat worku Lemma <tamworku1@gmail.com>
2022-08-26 01:16:58 +0200

    added error text && enabled CORS && added makefile

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 from application import routes
--- application/static/script.js
+++ application/static/script.js
@@ -1,16 +1,15 @@
 var baseUrl = 'http://localhost:5000/api';
 
 async function loadGraph() {
-
+	
 	let res = await fetch(baseUrl + '/data');
-
-	let graphData = await res.json();
-	if (graphData.error) {
-		let errorArea = document.getElementById('api-error');
-		errorArea.innerHTML = graphData.error;
-		console.log(graphData.error);
+	
+	if (!res.ok) {
+		let errorArea = document.getElementById('api_error');
+		errorArea.style = 'display: block';
 		return;
 	}
+	let graphData = await res.json();
 	let plotArea = document.getElementById("plotly-div");
 	Plotly.newPlot(plotArea, graphData, {});
 	console.log(graphData);
